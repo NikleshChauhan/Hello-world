2a9b8aa8f94bff00f90a1661b88942cd0959026f
Niklesh Chauhan <niklesh@propstack.com>
2016-12-26 15:02:48 +0530

    intial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+* 
+*/
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+	public function index($value='')
+	{
+		echo "hello world";
+	}
+}
+
+?>
